ab87c383741eba458efc1c6a3bdb86cf7cad9a8e
Aneesh-Subramanya <aneeshsubramanya@gmail.com>
2022-12-08 15:27:11 -0500

    Testing commit to main.

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include "Option.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    DivsTuple divs;    
+    Option o(OptionExercise::euro, OptionPayoff::call, OptionType::vanilla, 48., 50., .5, .25, .03, .0, divs, std::vector<double>());
+
+    return 0;
+}
