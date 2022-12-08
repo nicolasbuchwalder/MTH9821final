5547dd1c548eb3a77b5b10d6561fc5fcd80b9581
Aneesh-Subramanya <aneeshsubramanya@gmail.com>
2022-12-08 15:29:08 -0500

    Testing

--- main2.cpp
+++ main2.cpp
@@ -7,5 +7,6 @@ int main(int argc, const char * argv[]) {
     DivsTuple divs;    
     Option o(OptionExercise::euro, OptionPayoff::call, OptionType::vanilla, 48., 50., .5, .25, .03, .0, divs, std::vector<double>());
 
+    std::cout << "Done!" << std::endl;
     return 0;
 }
