c753f1679805803b054e5800aa8f608a95c77652
Aneesh-Subramanya <aneeshsubramanya@gmail.com>
2022-12-09 20:50:58 -0500

    Successful build

--- Option.cpp
+++ Option.cpp
@@ -8,15 +8,18 @@
 #include "Option.hpp"
 
 Option::Option(OptionExercise ex, OptionPayoff payoff, OptionType type, double S, double K, double T, double sigma, double r, double q, DivsTuple divs, std::vector<double> add_params)
-    : _ex(ex), _payoff(payoff), _type(type), _S(S), _K(K), _T(T), _sigma(sigma), _r(r), _q(q), _divs(divs), _add_params(add_params), _t(0) {
+    : _ex(ex), _payoff(payoff), _type(type), _S(S), _K(K), _T(T), _sigma(sigma), _r(r), _q(q), _divs(divs), _add_params(add_params), _t(0)
+{
     update_vals(0);
 };
 
-ParamsTuple Option::get_params() const {
+ParamsTuple Option::get_params() const
+{
     return std::make_tuple(_ex, _payoff, _type, _S, _K, _T, _sigma, _r, _q, _divs, _add_params);
 }
 
-void Option::update_vals(double t) {
+void Option::update_vals(double t)
+{
     _t = t;
 
     _d1 = (std::log(_S / _K) + (_r - _q + _sigma * _sigma / 2.) * (_T - _t)) / (_sigma * std::sqrt(_T - _t));
@@ -32,42 +35,48 @@ void Option::update_vals(double t) {
 };
 
 // CDF of std normal
-double Option::phi(double t) const {
+double Option::phi(double t) const
+{
     return std::erfc(-t / std::sqrt(2.)) / 2.;
 };
 
 // PDF of std normal
-double Option::z(double t) const {
+double Option::z(double t) const
+{
     return std::exp(-t * t / 2.) / std::sqrt(2. * std::numbers::pi);
 }
 
-std::vector<double> Option::price_european(bool includeGreeks = false) const {
+std::vector<double> Option::price_european(bool includeGreeks /*= false*/) const
+{
     std::vector<double> res;
 
-    switch (_payoff) {
-        case call:
-            res.push_back(_S * _q_disc * _Nd1 - _K * _r_disc * _Nd2);  // price
-            if (includeGreeks) {
-                res.push_back(_q_disc * _Nd1);                                                                                                     // delta
-                res.push_back(_q_disc / (_S * _sigma * std::sqrt(_T - _t)) * _zd1);                                                                // gamma
-                res.push_back(_S * _q_disc * std::sqrt(_T - _t) * _zd1);                                                                           // vega
-                res.push_back(-(_S * _sigma * _q_disc) / (2. * std::sqrt(_T - _t)) * _zd1 + _q * _S * _q_disc * _Nd1 - _r * _K * _r_disc * _Nd2);  // theta
-                res.push_back(_K * (_T - _t) * _r_disc * _Nd2);                                                                                    // rho
-            }
-            break;
+    switch (_payoff)
+    {
+    case call:
+        res.push_back(_S * _q_disc * _Nd1 - _K * _r_disc * _Nd2); // price
+        if (includeGreeks)
+        {
+            res.push_back(_q_disc * _Nd1);                                                                                                    // delta
+            res.push_back(_q_disc / (_S * _sigma * std::sqrt(_T - _t)) * _zd1);                                                               // gamma
+            res.push_back(_S * _q_disc * std::sqrt(_T - _t) * _zd1);                                                                          // vega
+            res.push_back(-(_S * _sigma * _q_disc) / (2. * std::sqrt(_T - _t)) * _zd1 + _q * _S * _q_disc * _Nd1 - _r * _K * _r_disc * _Nd2); // theta
+            res.push_back(_K * (_T - _t) * _r_disc * _Nd2);                                                                                   // rho
+        }
+        break;
 
-        case put:
-            res.push_back(-_S * _q_disc * (1. - _Nd1) + _K * _r_disc * (1. - _Nd2));  // price
-            if (includeGreeks) {
-                res.push_back(-_q_disc * (1 - _Nd1));                                                                                                          // delta
-                res.push_back(_q_disc / (_S * _sigma * std::sqrt(_T - _t)) * _zd1);                                                                            // gamma
-                res.push_back(_S * _q_disc * std::sqrt(_T - _t) * _zd1);                                                                                       // vega
-                res.push_back(-(_S * _sigma * _q_disc) / (2. * std::sqrt(_T - _t)) * _zd1 - _q * _S * _q_disc * (1 - _Nd1) + _r * _K * _r_disc * (1 - _Nd2));  // theta
-                res.push_back(-_K * (_T - _t) * _r_disc * (1 - _Nd2));                                                                                         // rho
-            }
-            break;
-        default:
-            break;
+    case put:
+        res.push_back(-_S * _q_disc * (1. - _Nd1) + _K * _r_disc * (1. - _Nd2)); // price
+        if (includeGreeks)
+        {
+            res.push_back(-_q_disc * (1 - _Nd1));                                                                                                         // delta
+            res.push_back(_q_disc / (_S * _sigma * std::sqrt(_T - _t)) * _zd1);                                                                           // gamma
+            res.push_back(_S * _q_disc * std::sqrt(_T - _t) * _zd1);                                                                                      // vega
+            res.push_back(-(_S * _sigma * _q_disc) / (2. * std::sqrt(_T - _t)) * _zd1 - _q * _S * _q_disc * (1 - _Nd1) + _r * _K * _r_disc * (1 - _Nd2)); // theta
+            res.push_back(-_K * (_T - _t) * _r_disc * (1 - _Nd2));                                                                                        // rho
+        }
+        break;
+    default:
+        break;
     }
     return res;
 };
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 //  Created by Nicolas Buchwalder on 28.11.22.
 //
 
+/*
 #include <iostream>
 #include <iomanip>
 #include "Option.hpp"
@@ -21,18 +22,36 @@ void PrintVector(const std::vector<double>& vec) {
 
 
 int main(int argc, const char * argv[]) {
-    
+
     DivsTuple divs;
     divs.push_back(std::make_tuple(true, 2./12., .01));
-    
+
     Option o(OptionExercise::euro, OptionPayoff::call, OptionType::vanilla, 48., 50., .5, .25, .03, .01, DivsTuple(), std::vector<double>());
-    
-    
+
+
     FiniteDifference fd;
     fd.set_params(o, 1000, 0.4);
     auto res = fd.price_option(Scheme::cn_lu, true);
     //fd.show_domain_params();
     //fd.show_grid(false);
     fd.print(res);
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <iomanip>
+#include "Option.hpp"
+#include "MonteCarlo.hpp"
+
+int main(int argc, const char *argv[])
+{
+
+    DivsTuple divs;
+    std::vector<double> add_params;
+    Option o(OptionExercise::euro, OptionPayoff::call, OptionType::vanilla, 48., 50., .5, .25, .03, .0, divs, add_params);
+
+    MonteCarlo mc;
+    std::cout << "Done!" << std::endl;
     return 0;
 }
